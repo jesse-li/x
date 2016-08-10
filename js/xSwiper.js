626d41fdd688b619eedecbfb3887399c3d8a1a81
jesse-li <569167508@qq.com>
2016-08-10 16:56:08 +0800

    v0.4

--- js/xSwiper.js
+++ js/xSwiper.js
@@ -1,3 +1,6 @@
+window.alert=function(txt){//处理safari中alter导致touch出错的问题（由于暂时还没找到解决的办法，先这样处理）
+	console.log(txt)
+}
 var xSwiper = function(option) {
 	var _this = this;//实例化xSwiper对象
 	_this.obody = option.obody;//轮播主体
@@ -11,9 +14,9 @@ var xSwiper = function(option) {
 	_this.autospeed = option.autospeed||2000;
 	//_this.thatIndex = option.thatIndex;//传入一个Number类型，直接轮播到当前的index
 	//_this.t = true;//轮播控制器，控制频繁点击的情况(应该不需要用户设置，，，，考虑私有化)
-	_this._init();
 	_this.speed = option.speed||700;
-
+	_this.chaos = option.chaos;
+	_this._init();
 }
 xSwiper.prototype = {
 	_init: function() {//_init方法调用一些主要的函数
@@ -26,6 +29,7 @@ xSwiper.prototype = {
 		_this._startF();
 		_this._moveF();
 		_this._endF();
+		_this._cancel();
 		_this.close = false;
 		//_this._first(_this.thatIndex);//不需要用户来传，用户只要调用实例对象的_first方法就可以进行指定滚动
 		_this._resizeF();
@@ -88,19 +92,43 @@ xSwiper.prototype = {
 		}
 		_this.obody.width(_this.ochild.length * _this.width);
 		_this.ochild.width(_this.width);
-		
 		if(!_this.css3){//如果不使用css3滚动，需要设置外层的高度
 			_this.obody.parent().height(_this.obody.height())
+		}
+		if(_this.chaos){//2016-08-10混乱模式处理 （苦逼的处理比例不一样的图片）
+			var img = _this.obody.find("img");
+			_this.obody.parent().height(_this.chaos);
+			_this.ochild.height(_this.chaos)
+			for(var i=0,j=img.length;i<j;i+=1){
+				if(img[i].complete){
+					var marTop = Math.abs($(img[i]).height()-_this.chaos)/2;
+					if($(img[i]).height()>_this.chaos){
+						$(img[i]).css("margin-top",-marTop);
+					}else{
+						$(img[i]).css("margin-top",marTop);
+					}
+				}else{
+					(function(k){
+						$(img[k]).on("load",function(){
+							var marTop = Math.abs($(img[k]).height()-_this.chaos)/2;
+							if($(img[k]).height()>_this.chaos){
+								$(img[k]).css("margin-top",-marTop);
+							}else{
+								$(img[k]).css("margin-top",marTop);
+							}
+						})
+					})(i)
+					
+				}
+			}
 		}
 	},
 	_startF: function() {//绑定主体的touchstart事件，用于计算滑动的初始X坐标
 		var _this = this;
 		_this.obody.on("touchstart",
 			function(e) {
-				//clearTimeout(_this.timerAuto);
-				e.preventDefault();
-				e.stopPropagation();
 				_this.startX = e.originalEvent.targetTouches[0].pageX;
+				_this.startY = e.originalEvent.targetTouches[0].pageY;
 				_this.close = false;
 			});
 	},
@@ -113,6 +141,7 @@ xSwiper.prototype = {
 					_this._autoPlayF();
 				}
 				_this.moveX = e.originalEvent.targetTouches[0].pageX;
+				_this.moveY = e.originalEvent.targetTouches[0].pageY;
 				_this.close = true;
 			})
 
@@ -124,18 +153,21 @@ xSwiper.prototype = {
 		});
 
 	},
+	_cancel:function(){
+		var _this = this;
+		_this.obody.on("touchcancel", function(e) {
+			e.preventDefault();
+		});
+	},
 	_movefution: function(e) {//具体的运动方法
 		var _this = this;
-		e.preventDefault();
-		e.stopPropagation();
-		//if (_this.t && _this.close) {//控制器，必须用户滑动结束且上一次运动结束才调用 2016-7-29修改点击和滑动的判定
 		
-			if(Math.abs(_this.moveX-_this.startX)>100&&_this.close){
+			if(Math.abs(_this.moveX-_this.startX)>50&&_this.close){//控制器，必须用户滑动结束且上一次运动结束才调用 2016-7-29修改点击和滑动的判定
 				if(_this.t){
 					_this.t = false;
-					//_this.close = false;
+					
 					if (_this.moveX && _this.moveX > _this.startX) {//判断滑动的方向
-				//console.log("右滑");
+				
 						if (_this.css3) {//使用css3
 							if (_this.index === 0) {//通过index来判断是否该滑动
 								_this.t = true;
@@ -162,7 +194,7 @@ xSwiper.prototype = {
 							}
 		
 						} else {//使用传统的定位进行运动
-							/*if (parseInt(_this.obody.css("left")) >= 0 || !parseInt(_this.obody.css("left"))) {*/
+							
 								if(_this.index === 0){
 								setTimeout(function() {
 										_this.t = true;
@@ -198,9 +230,9 @@ xSwiper.prototype = {
 						}
 
 			} else if (_this.moveX && _this.moveX < _this.startX) {
-				//console.log("左滑");
+				
 				if (_this.css3) {
-					/*if (_this.index * _this.ochild.width() >= _this.ochild.length * _this.ochild.width() - _this.ochild.parent().parent().width()) {*/
+					
 						if(_this.index >= _this.ochild.length-1){
 						setTimeout(function() {
 								_this.t = true;
@@ -229,7 +261,7 @@ xSwiper.prototype = {
 					}
 
 				} else {
-					/*if (parseInt(_this.obody.css("left")) <= -(_this.ochild.length * _this.ochild.width() - _this.ochild.parent().parent().width())) {*/
+					
 						if(_this.index >= _this.ochild.length-1){
 						_this.t = true;
 						return;
@@ -263,30 +295,36 @@ xSwiper.prototype = {
 			}
 		}
 		}else {
-			if (_this.bindclick && typeof(_this.bindclick) === "function") {//绑定主体点击事件
-				_this.bindclick(_this.index);
-
+			
+			if(Math.abs(_this.moveY-_this.startY)<5||!_this.close){
+				if (_this.bindclick && typeof(_this.bindclick) === "function") {//绑定主体点击事件
+					_this.bindclick(_this.index);
+	
+				}
 			}
+			
 		} 
 
 	},
 	_resizeF: function() {//绑定页面resize事件，触发后轮播初始化
-		var _this = this;
+		var _this = this,nowWidth=$(window).width();
 		$(window).on("resize",
 			function() {
-				_this._widthF();
-				_this.index = 0;
-				_this.ochild.width(_this._width);
-				if (_this.css3) {
-					_this.obody.css("transform", "translate3d(0px,0px,0px)");
+				if(nowWidth != $(window).width()){
+					nowWidth = $(window).width();
+					_this._widthF();
+					_this.index = 0;
+					_this.ochild.width(_this._width);
+					if (_this.css3) {
+						_this.obody.css("transform", "translate3d(0px,0px,0px)");
+					}
+					_this.obody.css("left", "0");
+					if (_this.succ && typeof(_this.succ) === "function") {
+						_this.succ(_this.index);
+	
+					}
 				}
-				_this.obody.css("left", "0");
-				if (_this.succ && typeof(_this.succ) === "function") {
-					_this.succ(_this.index);
-
-				}
-
-
+				
 			})
 
 
